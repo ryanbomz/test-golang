08924112634d83094c4a1f1b016b8e0c26e71653
ryanbomz <73924698+ryanbomz@users.noreply.github.com>
2022-07-26 14:44:12 +0700

    Add files via upload

---
+++ icp-planner-main/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jackthepanda96/icp-planner/controller"
+	"github.com/jackthepanda96/icp-planner/model"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+var (
+	listData []model.User
+)
+
+func main() {
+	e := echo.New()
+	fmt.Println("Hello World")
+
+	e.Use(middleware.CORS())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: `[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency_human}` + "\n",
+	}))
+	e.Use(middleware.RemoveTrailingSlash())
+
+	um := model.UserModel{}
+	uc := controller.UserController{Model: um}
+	bm := model.BalanceModel{}
+	bc := controller.BalanceController{Model: bm}
+
+	e.GET("/users", uc.GetAllUSer())
+	e.POST("/users", uc.Register())
+	e.POST("/login", uc.Login())
+	e.PUT("/users/:id", uc.UpdateProfile(), middleware.BasicAuth(func(username, password string, ctx echo.Context) (bool, error) {
+		if _, err := um.Login(username, password); err != nil {
+			return false, nil
+		}
+		return true, nil
+	}))
+	e.POST("/income", bc.Income(), middleware.BasicAuth(func(username, password string, ctx echo.Context) (bool, error) {
+		if _, err := um.Login(username, password); err != nil {
+			return false, nil
+		}
+		return true, nil
+	}))
+	e.POST("/expense", bc.Expense(), middleware.BasicAuth(func(username, password string, ctx echo.Context) (bool, error) {
+		if _, err := um.Login(username, password); err != nil {
+			return false, nil
+		}
+		return true, nil
+	}))
+	e.GET("/balance", bc.Balance(), middleware.BasicAuth(func(username, password string, ctx echo.Context) (bool, error) {
+		if _, err := um.Login(username, password); err != nil {
+			return false, nil
+		}
+		return true, nil
+	}))
+
+	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
+}
---
+++ icp-planner-main/model/user.go
@@ -0,0 +1,109 @@
+package model
+
+import (
+	"errors"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	listData []User
+)
+
+func init() {
+	listData = []User{}
+}
+
+type User struct {
+	ID       int
+	Nama     string `json:"nama"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type UserModel struct{}
+
+func GenerateID(last int) int {
+	if last > 0 {
+		last++
+		return last
+	}
+	return 1
+}
+
+func (um *UserModel) Insert(newUser User) (User, error) {
+	var found int = -1
+	if len(balanceHistory) == 0 {
+		newUser.ID = GenerateID(0)
+	} else {
+		newUser.ID = GenerateID(listData[len(listData)-1].ID)
+	}
+
+	for idx, val := range listData {
+		if val.Email == newUser.Email {
+			found = idx
+		}
+	}
+
+	if found != -1 {
+		return User{}, errors.New("duplicate user")
+	}
+
+	encryptPass, _ := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
+	newUser.Password = string(encryptPass)
+	listData = append(listData, newUser)
+
+	return newUser, nil
+}
+
+func (um *UserModel) Update(updatedUser User) (User, error) {
+	use := -1
+	for idx, val := range listData {
+		if val.ID == updatedUser.ID {
+			use = idx
+		}
+	}
+
+	if use == -1 {
+		return User{}, errors.New("no data found")
+	}
+
+	if updatedUser.Nama != "" {
+		listData[use].Nama = updatedUser.Nama
+	}
+
+	if updatedUser.Email != "" {
+		listData[use].Email = updatedUser.Email
+	}
+
+	return listData[use], nil
+}
+func (um *UserModel) GetAll() ([]User, error) {
+	if len(listData) == 0 {
+		return nil, errors.New("no record on database")
+	}
+	return listData, nil
+}
+
+func (um *UserModel) Login(email string, password string) (User, error) {
+	log.Println(email, password, listData)
+	var res User
+	for _, val := range listData {
+		if val.Email == email {
+			res = val
+		}
+	}
+
+	if res.ID == 0 {
+		return User{}, errors.New("no data found")
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(res.Password), []byte(password))
+	if err != nil {
+		return User{}, errors.New("wrong password")
+	}
+
+	return res, nil
+
+}
